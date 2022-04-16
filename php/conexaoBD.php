c021a523d99f78b8d8e4d13e3a58d0161f485a0d
andreiaaf <67799694+andreiaaf@users.noreply.github.com>
2022-04-16 16:19:02 -0300

    inicial

---
+++ php/conexaoBD.php
@@ -0,0 +1,16 @@
+<?php 
+
+$servername = "localhost";
+$username = "root";
+$password = "robsten0913";
+$dbname = "sisDoencas_aruja";
+$conexao = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conexao->connect_error){
+        die("Connection failed: " .$conexao->connect_error);
+    }
+$conexao->set_charset("utf8");
+    if($conexao->connect_error){
+        die("Connection failed: " . $conexao->connect_error);
+    }
+?>
